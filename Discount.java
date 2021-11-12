b468bbe308c4f3e9f6c0b555ae4a54e565db4901
Myriamchtf <91917709+Myriamchtf@users.noreply.github.com>
2021-11-12 13:08:23 +0100

    Create Discount.java

---
+++ Discount.java
@@ -0,0 +1,15 @@
+public class Discount {
+
+	private static long value_discount = 0;
+	
+	public Discount(long value_discount) {
+		// TODO Auto-generated constructor stub
+		this.value_discount = value_discount;
+	}
+	
+	public static long getValue_discount() {
+		return value_discount;
+	}
+	
+
+}
