bf643b03cbdba815eef2934346ee48498451184c
Myriamchtf <91917709+Myriamchtf@users.noreply.github.com>
2021-11-12 13:06:45 +0100

    Create Vehicule.java

---
+++ Vehicule.java
@@ -0,0 +1,13 @@
+public interface Vehicule {
+	
+
+	public String getBrand();
+	public long getValue();
+	public String toString();
+	
+	/*public static void GetPrixApresSolde(int i) {
+		long newValue  = getValue() - Discount.getValue_discount();
+	}*/
+	
+
+}
